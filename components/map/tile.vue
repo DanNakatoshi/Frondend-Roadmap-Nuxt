4faa2cc3520cd500537280e6d444c818f685e1fe
TraitOtaku <traitotaku@gmail.com>
2023-01-11 13:05:54 +0900

    menu

--- components/map/tile.vue
+++ components/map/tile.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="cube">
+  <div class="cube" @mouseover="showTooltip">
     <div class="side front"></div>
     <div class="side right"></div>
     <div class="side top" :class="tileTop">
       <slot></slot>
     </div>
     <div class="side bottom"></div>
-    <!-- <TooltipMenu/> -->
+    <TooltipMenu/>
   </div>
 </template>
 
 <script setup>
 const prop = defineProps(["tileTop"]);
+
+
 </script>
--- components/tooltip/menu.vue
+++ components/tooltip/menu.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="tooltip-wrapper">
-    <div>menu1</div>
-    <div>menu1</div>
-    <div>menu1</div>
-    <div>menu1</div>
+    <div class="tooltip-postion">
+      <div>menu1</div>
+      <div>menu2</div>
+      <div>menu3</div>
+      <div>menu4</div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .tooltip-wrapper {
-  top: 200px;
-  left: 300px;
+  position: relative;
+  .tooltip-postion {
+    transition: all 0.5s;
+    opacity: 0;
+    position: absolute;
+    bottom: 90px;
+    left: 0px;
+    z-index: 101;
+  }
 }
 </style>
